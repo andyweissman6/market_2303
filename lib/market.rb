d42cbb4b68470c9898ccf1062eedbdd119d6fe94
Andy Weissman <andyweissman6@gmail.com>
2023-04-17 10:45:02 -0600

    created class, added add_vendor, vendor_names, vendors_that_sell methods

---
+++ lib/market.rb
@@ -0,0 +1,28 @@
+class Market
+  attr_reader :name,
+              :vendors
+  def initialize(name)
+    @name = name
+    @vendors = Array.new(0)
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all do |vendor|
+      vendor.inventory.keys.include?(item)
+    end
+  end
+
+
+
+
+end
